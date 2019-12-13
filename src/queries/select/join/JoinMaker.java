6ba1ea5e74222225d5fa9ea251b7c0bf9ff7adcb
m_gohari <goharinezhad.2019@gmail.com>
2019-12-13 23:56:05 +0330

    maker for creating join in query

--- src/queries/select/join/JoinMaker.java
+++ src/queries/select/join/JoinMaker.java
@@ -1,27 +1,21 @@
 package queries.select.join;
 
-public class JoinMaker {
+public class JoinMaker extends JoinConstant {
+    public static String leftJoin(){
+        return "";
+    }
+    public static String rightJoin(){
+        return "";
+    }
+    public static String join(String joinCondition){
+        return "";
 
-//    private final static String leftJoin="left join";
-//    private final static String innerJoin="inner join";
-//    private final static String join="join";
-//    private final  String outerJoin="outer Join";
-//    private final String rightJoin="right join";
-    JoinMaker(){};
-    public static final String leftJoin (){
-        return "left Join";
     }
-    public static final String join(){
-        return "join";
+    public static String outerJoin(){
+        return "";
     }
-    public static final String innerJoin(){
-        return "inner join";
-    }
-    public static final String outerJoin(){
-        return "outer join";
-    }
-    public static final String rightJoin(){
-        return "right join";
+    public static String innerJoin(){
+        return "";
     }
 
 }
