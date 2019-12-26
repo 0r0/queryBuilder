ccc80f5189817e39951dd82612004b0cc5b38606
m_gohari <goharinezhad.2019@gmail.com>
2019-12-26 12:13:42 +0330

    add left join ,right join,join ..

--- src/queries/select/join/JoinMaker.java
+++ src/queries/select/join/JoinMaker.java
@@ -1,21 +1,35 @@
 package queries.select.join;
 
-public class JoinMaker extends JoinConstant {
-    public static String leftJoin(){
-        return "";
+
+
+public class JoinMaker extends JoinConstant  {
+
+    public static String leftJoin(String[] table1,String[] table2){
+        stringArraySizeChecker(table1,table2);
+        return leftJoinConstant()+" "+table2[0]+" on "+table2[0]+"."+table2[1]+" = "+table1[0]+"."+table1[1];
     }
-    public static String rightJoin(){
-        return "";
+    public static String rightJoin(String[] table1,String[] table2){
+        stringArraySizeChecker(table1,table2);
+        return rightJoinConstant()+" "+table2[0]+" on "+table2[0]+"."+table2[1]+" = "+table1[0]+"."+table1[1];
     }
-    public static String join(String joinCondition){
-        return "";
+    public static String join(String[] table1,String[] table2){
+        stringArraySizeChecker(table1,table2);
+
+        return joinConstant()+" "+table2[0]+" on "+table2[0]+"."+table2[1]+" = "+table1[0]+"."+table1[1];
 
     }
-    public static String outerJoin(){
-        return "";
+    public static String outerJoin(String[] table1,String[] table2){
+        stringArraySizeChecker(table1,table2);
+        return outerJoinConstant()+" "+table2[0]+" on "+table2[0]+"."+table2[1]+" = "+table1[0]+"."+table1[1];
     }
-    public static String innerJoin(){
-        return "";
+    public static String innerJoin(String[] table1,String[] table2){
+        stringArraySizeChecker(table1,table2);
+        return innerJoinConstant()+" "+table2[0]+" on "+table2[0]+"."+table2[1]+" = "+table1[0]+"."+table1[1];
+    }
+    private static void stringArraySizeChecker(String[] table1, String[] table2){
+        if(table1.length!=2 || table2.length!=2)
+            throw new IllegalArgumentException("size of String array must be 2, first is table name, second is field  name");
+
     }
 
 }
