16a473c488aaee2bbcb9293c2b7e623962588631
0r0 <0r0.developer@gmail.com>
2019-12-27 02:00:43 +0330

    add jdbc singleton prototype

--- src/connection/JDBCConnection.java
+++ src/connection/JDBCConnection.java
@@ -1,5 +1,17 @@
 package connection;
 
 public class JDBCConnection {
+    private static JDBCConnection object=null;
+    private JDBCConnection(){}
+    public static JDBCConnection getConnection(){
+        if(object==null)
+            object=new JDBCConnection();
+        return object;
+    }
 
+    public static void connectivity(){
+
+    }
 }
+
+
