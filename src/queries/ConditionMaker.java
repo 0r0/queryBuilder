70b9e6f393e9e09ee7fd3f909df51f6e3e5464a1
m_gohari <goharinezhad.2019@gmail.com>
2019-12-26 12:11:19 +0330

    add operator and modify it with Condition

--- src/queries/ConditionMaker.java
+++ src/queries/ConditionMaker.java
@@ -5,18 +5,20 @@ package queries;
 public class ConditionMaker {
     private String orConditionStatement;
     private String andConditionStatement;
+    private String[] operators={"=",">",">=","<=","<","<>"};
 
 
-    public ConditionMaker orCondition(String condition1, String condition2) {
+    public ConditionMaker orCondition(String condition1,String operator,String condition2 ) {
 
-        this.orConditionStatement = "or " + condition1 + "=" + condition2;
+        this.orConditionStatement = "or " + condition1 + ""+operator+"" + condition2;
         return this;
     }
 
-    ;
 
-    public ConditionMaker andCondition(String condition1, String condition2) {
-        this.andConditionStatement = "and " + condition1 + "=" + condition2;
+
+    public ConditionMaker andCondition(String condition1,String operator, String condition2) {
+
+        this.andConditionStatement = "and " + condition1 + ""+operator+""+ condition2;
         return this;
 
     }
@@ -27,8 +29,29 @@ public class ConditionMaker {
         return this.orConditionStatement;
     }
 
+
+
     public String getAndConditionStatement() {
         return this.andConditionStatement;
+    }
+    private boolean conditionOperatorChecker(String operator){
+        for (String s : operators) {
+            if (operator.equals(s))
+                return true;
+        }
+        return false;
+    }
+    private String getCondition(String condition1,String operator, String condition2) {
+
+
+        if(conditionOperatorChecker(operator))
+            return condition1 + ""+operator+""+ condition2;
+        else
+            throw new IllegalArgumentException("the operand is not true!");
+
+
+
+
     }
 
 }
