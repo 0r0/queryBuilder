f8ded492d70c8a6b91b2673af193f433d390dd14
0r0 <0r0.developer@gmail.com>
2019-12-29 01:44:02 +0330

    proto typing all kinda select forms

--- src/queries/select/Select.java
+++ src/queries/select/Select.java
@@ -1,9 +1,26 @@
 package queries.select;
 
 import Makers.Where;
-import queries.Queries;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Select extends SelectAbstract implements Where{
+    //make connection
+    //send select query to executor
+    //return all result
+
+  public boolean select(String firstTable, HashMap<String,String> selectedTableField, ArrayList<String> joins,String condition){
+      return true;
+  }
+  public boolean select(String firstTable,HashMap<String,String> selectedTableField,ArrayList<String> joins){
+        return true;
+        }
+        public boolean select(String firstTable,HashMap<String,String> selectedTableField){
+      return true;
+        }
+
 
 
 }
